f67b9dbaed7ff36bf3d42846ae2a56faf2a06bb9
Kidslife <kidpres666@gmail.com>
2017-04-20 11:58:16 +0300

    Add files via upload

---
+++ SomeCircle.java
@@ -0,0 +1,12 @@
+package firstLtask1;
+
+public class SomeCircle {
+	double radius;
+	double L;
+	double S;
+
+
+	public String toString(){
+		return "Радіус вашого круга = " + radius + "; його площа = " + S + " а периметр = " + L;
+	}
+}
